5c38492cdd7e82714d804b4d04ceb9ed8c40c76d
shayanbagchi <bagchishayan.9@gmail.com.com>
2019-04-22 23:13:04 +0530

    Tiny Tweeks.

--- server.js
+++ server.js
@@ -126,5 +126,5 @@ app.post('/auth', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log('server is running on port 3000');
+	console.log(`server is running on port ${process.env.PORT}`);
 })
